e4e465d07954535a2f2cd4de5a7ce7f5e2d2305c
AIRa2025-ai <osvobozhdenie.ra@gmail.com>
2025-11-24 20:59:27 +0300

    Update ra_config.py

--- modules/ra_config.py
+++ modules/ra_config.py
@@ -1,4 +1,4 @@
 # modules/ra_config.py
 # Здесь можно хранить конфиги и константы для РаСвет
-ARCHIVE_URL = "https://example.com/rasvet_archive.zip"
+ARCHIVE_URL = "https://mega.nz/file/FlQ0ET4J#9gJjCBnj5uYn5bJYYMfPiN3BTvWz8el8leCWQPZvrUg"
 TIMEOUT = 120
