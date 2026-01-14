fbc788870f590fc575744d951b21690adc3a2bb2
AIRa2025-ai <osvobozhdenie.ra@gmail.com>
2026-01-14 16:15:07 +0300

    Update run_ra_all.sh

--- run_ra_all.sh
+++ run_ra_all.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Твой токен Telegram
 export BOT_TOKEN="7304435178:AAFzVnyQVtBMiYMXDvShbfcyPDw1_JnPCFM"
 
-# --- Поднимаем CORE в фоне ---
-python3 run_ra_core.py &
+cd ~/iskin_ra || exit 1
 
-# Даем CORE пару секунд для старта
-sleep 3
-
-# --- Поднимаем Telegram-бота из core ---
-cd core || exit 1
-python3 -m ra_bot_gpt
+python3 -m core.ra_bot_gpt
