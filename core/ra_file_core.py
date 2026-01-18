5361c76c0f68e66711de5b179d2e7ce78860ff12
AIRa2025-ai <osvobozhdenie.ra@gmail.com>
2026-01-19 01:47:30 +0300

    Create ra_file_core.py

---
+++ core/ra_file_core.py
@@ -0,0 +1,6 @@
+class RaFileCore:
+    def __init__(self, manager, consciousness, writer, repo):
+        self.manager = manager
+        self.consciousness = consciousness
+        self.writer = writer
+        self.repo = repo
