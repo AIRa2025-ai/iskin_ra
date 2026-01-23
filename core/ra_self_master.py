34865a12976cecc328c369ffa682531ccec1f9a6
AIRa2025-ai <osvobozhdenie.ra@gmail.com>
2026-01-23 19:20:22 +0300

    Update ra_self_master.py

--- core/ra_self_master.py
+++ core/ra_self_master.py
@@ -20,7 +20,6 @@ from modules.ra_file_consciousness import RaFileConsciousness
 from modules.logs import log_info
 from modules.security import log_action
 from modules.ra_world_observer import RaWorld
-from modules.ra_nervous_system import RaNervousSystemModule
 
 # -------------------------------
 # Police модуль (опционально)
@@ -99,6 +98,7 @@ class RaSelfMaster:
         self.world.set_event_bus(self.event_bus)
         
         # Нервная система
+        from modules.ra_nervous_system import RaNervousSystemModule
         self.nervous_module = RaNervousSystemModule(self, self.event_bus)
         self._create_bg_task(self.nervous_module.start(), "nervous_module")
         
