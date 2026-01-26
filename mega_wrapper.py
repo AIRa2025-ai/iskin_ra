b6b885a0108ce8c2838e2160f89433e91bbd7ba2
AIRa2025-ai <osvobozhdenie.ra@gmail.com>
2026-01-26 19:42:03 +0300

    Update mega_wrapper.py

--- mega_wrapper.py
+++ mega_wrapper.py
@@ -1,12 +1,8 @@
 # mega_wrapper.py
-from mega import Mega  # обычный синхронный Mega-клиент
+from mega.mega import Mega  # синхронный Mega-клиент
 
 def upload_file_sync(email, password, local_path):
-    try:
-        m = Mega()
-        m.login(email, password)
-        uploaded_file = m.upload(local_path)
-        return uploaded_file
-    except Exception as e:
-        print(f"❌ Ошибка загрузки файла {local_path} в Mega: {e}")
-        return None
+    m = Mega()
+    m.login(email, password)
+    file = m.upload(local_path)
+    return file
